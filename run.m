ea0f78d5163c21b2783c32215cc6bddf5c2b79ba
samleeney <91542185+samleeney@users.noreply.github.com>
2021-09-28 14:45:04 +0100

    Initial Commit

--- run.m
+++
@@ -1,8 +0,0 @@
-image_filepath = 'example_image.png'; %enter image location(s) here
-I = classify_LT(image_filepath);
-
-figure(1)
-subplot(1,2,1)
-imshow(I) %show classification
-subplot(1,2,2)
-imshow(imresize(imread(image_filepath),[256,256])) %show original image
